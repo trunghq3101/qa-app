4649a58313d6577f947e8d1678ee011f6d980a7a
Trung Hoàng <trunghq3101@gmail.com>
2018-11-27 11:26:10 +0700

    add Question comp + basic Style

---
+++ client/src/components/Question/Question.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default (props) => (
+    <div className="mt-3">
+        <h3>How to drive a motorbike properly?</h3>
+        <p>I am 20 years old and I haven't drove any motorbike. My new girlfriend told me that she wants me to take her to go for a trip on the motorbike. I have 1 week to the D-day, can anyone help me?</p>
+        <p><small className="text-muted">Question Info</small></p>
+    </div>
+)
--- client/src/containers/QuestionHolder/QuestionHolder.jsx
+++ client/src/containers/QuestionHolder/QuestionHolder.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
+import Question from '../../components/Question/Question';
 
 export default class QuestionHolder extends Component {
     render() {
         return (
             <div className="row mt-2 justify-content-center">
                 <div className="col-12 col-sm-10">
-                    <div>Question</div>
-                    <div>Answers</div>
-                    <div>Controls</div>
-                    <div>Answer Form</div>
+                    <div className="card px-3 py-2">
+                        <Question />
+                        <div>Controls</div>
+                        <div>Answer Form</div>
+                        <div>Answers</div>
+                    </div>
                 </div>
             </div>
         )
