831bb1996eaedbf63f23ffb8537ad965dc24bbc3
Trung Hoàng <trunghq3101@gmail.com>
2018-12-07 10:00:20 +0700

    test playground

--- client/src/playground/PlayGround.jsx
+++ client/src/playground/PlayGround.jsx
@@ -1,45 +1,42 @@
-import React, { Component } from 'react'
-import Axios from 'axios';
+import React, { Component, PureComponent } from 'react'
 
-class ChildComp extends Component {
-
-    clickedHandler = (e) => {
-        e.preventDefault()
-        Axios.get("/api/q/all")
-            .then(res => console.log("fetched"))
-    }
+class ChildComp extends PureComponent {
 
     componentWillUpdate() {
         console.log("Child will update")
     }
 
     render() {
+        console.log("Child render")
         return (
             <div>
-                <h1>{this.props.text}</h1>
-                <button onClick={this.clickedHandler}>Fetch</button>
+                {this.props.value}
             </div>
         )
     }
 }
 
-const withControl = (WrappedComp) => {
-    return class extends Component {
-        render() {
-            return (
-                <WrappedComp {...this.props} />
-            )
-        }
-    }
-}
-
-const ChildWithControl = withControl(ChildComp)
-
 export default class PlayGround extends Component {
+    state = {
+        list: new Array(1000).fill(0).map((v, i) => i)
+    }
+
+    clickedHandler = () => {
+        this.setState(prevState => ({
+            list: [...prevState.list, prevState.list[prevState.list.length-1] + 1]
+        }))
+    }
+
+    componentWillUpdate() {
+        console.log("Playground will update")
+    }
+
     render() {
+        console.log("Playground render")
         return (
             <div style={{ paddingTop: "60px" }}>
-                <ChildWithControl text="Hello"/>
+                <button onClick={this.clickedHandler}>Change state</button>
+                {this.state.list.map(item => <ChildComp key={item} value={item}/>)}
             </div>
         )
     }
