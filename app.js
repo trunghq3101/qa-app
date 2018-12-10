e1ce3f96f9047db6f46fcfbd6e678e5a366d88fc
Trung Hoàng <trunghq3101@gmail.com>
2018-12-10 15:47:35 +0700

    add dotenv + use to load db

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const configEnv = require('dotenv').config()
 
 const mongooseInit = require('./config/mongooseInit');
 var apiRouter = require('./routes/api');
