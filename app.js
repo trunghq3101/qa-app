41d8ae1929656dd8f205224b51f700361fb00f10
Trung Hoàng <trunghq3101@gmail.com>
2018-12-10 14:57:20 +0700

    setup environment for deploy

--- app.js
+++ app.js
@@ -16,8 +16,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use('/api', apiRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- config/mongooseInit.js
+++ config/mongooseInit.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function mongooseInit() {
-    mongoose.connect("mongodb://localhost:27017/qa-app", { useNewUrlParser: true }, err => {
+    mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/qa-app", { useNewUrlParser: true }, err => {
         if (err) {
             console.log(err);
         } else {
